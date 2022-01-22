f8b36b211b8b857e397dc974d00c8bee027dc4b5
Daniele Ambrosino <mail@danieleambrosino.it>
2022-01-22 01:28:43 +0100

    feat: implement TokenExtractor

---
+++ src/TokenExtractor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DanieleAmbrosino\FirebaseAuthenticationBundle;
+
+use DanieleAmbrosino\FirebaseAuthenticationBundle\Contracts\TokenExtractorInterface;
+use InvalidArgumentException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\TokenNotFoundException;
+
+class TokenExtractor implements TokenExtractorInterface
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function extract(Request $request): string
+	{
+		if (!$request->headers->has('Authorization')) {
+			throw new TokenNotFoundException('Authorization header not found!');
+		}
+
+		$authorizationHeader = $request->headers->get('Authorization');
+		$headerParts = explode(' ', $authorizationHeader, 3);
+
+		if (count($headerParts) !== 2 || strcasecmp($headerParts[0], 'Bearer') !== 0) {
+			throw new InvalidArgumentException('Invalid authorization header!');
+		}
+
+		return $headerParts[1];
+	}
+}
