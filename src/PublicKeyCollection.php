26b45be7d41300f43b691fcdcd812792f68053a3
Daniele Ambrosino <mail@danieleambrosino.it>
2022-01-22 01:29:22 +0100

    feat: implement PublicKeyCollection

---
+++ src/PublicKeyCollection.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace DanieleAmbrosino\FirebaseAuthenticationBundle;
+
+use DanieleAmbrosino\FirebaseAuthenticationBundle\Contracts\PublicKeyCollectionInterface;
+use InvalidArgumentException;
+
+class PublicKeyCollection implements PublicKeyCollectionInterface
+{
+
+	/**
+	 * @var array<string,string>
+	 */
+	private array $collection = [];
+
+	/**
+	 * @param array $data The public keys indexed by ID.
+	 */
+	public function __construct(array $data)
+	{
+		foreach ($data as $key => $value) {
+			if (!is_string($key) || !is_string($value)) {
+				throw new InvalidArgumentException('Either keys and values in a PublicKeyCollection must be strings.');
+			}
+			$this->collection[$key] = $value;
+		}
+	}
+
+	public function get(string $id): string
+	{
+		if (!isset($this->collection[$id])) {
+			throw new InvalidArgumentException("Unable to find the public key with ID $id");
+		}
+		return $this->collection[$id];
+	}
+
+	public function set(string $id, string $value): PublicKeyCollectionInterface
+	{
+		$this->collection[$id] = $value;
+		return $this;
+	}
+
+	public function serialize(): string
+	{
+		return serialize($this->collection);
+	}
+
+	public function unserialize(string $data)
+	{
+		$this->collection = unserialize($data);
+	}
+}
